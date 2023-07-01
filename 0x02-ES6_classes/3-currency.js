7eb7a5a93d95e59ba6a6c1c5d46fadca9c76c4a5
flashingstars <annkarye@gmail.com>
2023-07-01 09:50:12 +0000

    Class methods

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,26 +1,28 @@
+/* eslint-disable no-underscore-dangle */
+
 export default class Currency {
   constructor(code, name) {
-  this._code = code;
-  this._name = name;
+    this._code = code;
+    this._name = name;
   }
 
   get code() {
-  return this._code;
+    return this._code;
   }
 
   get name() {
-  return this._name;
+    return this._name;
   }
 
   set code(value) {
-  this._code = value;
+    this._code = value;
   }
 
   set name(value) {
-  this._name = value;
+    this._name = value;
   }
 
   displayFullCurrency() {
-  return `${this._name} (${this._code})`;
+    return `${this._name} (${this._code})`;
   }
 }
