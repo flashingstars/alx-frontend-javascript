2951f20435d000829181af13230333ee2c1658d9
flashingstars <annkarye@gmail.com>
2023-07-01 11:05:25 +0000

    Inheriting from superclass building

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-underscore-dangle */
+
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    if (typeof floors === 'number') {
+      this._floors = floors;
+    } else {
+      throw new TypeError('floors must be a Number');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
